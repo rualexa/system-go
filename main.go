bb0d304b524f0038a73218399e45fec6abb2183b
Rubens Rangel <rubensrangjob802@gmail.com>
2023-02-07 12:32:04 -0400

    create function creaded done

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
 func main() {
 	http.HandleFunc("/", Inicio)
+	http.HandleFunc("/crear", Crear)
 	log.Println("servidor corriendo...")
 	http.ListenAndServe(":8080", nil)
 }
 
 func Inicio(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+	//fmt.Fprintf(w, "hello world")
+	plantillas.ExecuteTemplate(w, "inicio", nil)
+}
+
+func Crear(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "hello world")
+	plantillas.ExecuteTemplate(w, "crear", nil)
 }
